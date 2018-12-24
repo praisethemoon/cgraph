a07f50f011469147083cbf4d7e56af943439a293
praisethemoon <akaisora@tuta.io>
2018-12-24 18:02:34 +0100

    ++

---
+++ source/libcgraph/include/cg_math.h
@@ -0,0 +1,25 @@
+#ifndef CG_MATH_H
+#define CG_MATH_H
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <malloc.h>
+
+#include "cgraph.h"
+#include "cg_operation.h"
+#include "cg_types.h"
+#include "cg_variables.h"
+#include "cg_errors.h"
+#include "cg_constants.h"
+//#include "cg_enums.h"
+#include "cg_factory.h"
+
+CGNode* sigmoid(CGNode* x);
+CGNode* softmax(CGNode* x);
+CGNode* relu(CGNode* x);
+
+// log(1+ e^x)
+CGNode* smoothRelu(CGNode* x);
+
+#endif
